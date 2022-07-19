c84676d0dbf7600fbaf60809d90ce8cc88b03fed
Vibhor Kumar <vibhor4327@gmail.com>
2022-07-19 11:03:28 +0530

    first program upload

---
+++ Random_Password_Generator.java
@@ -0,0 +1,18 @@
+import java.util.Random;
+
+public class Random_Password_Generator {
+    public static void main(String[] args) {
+        Random shuffle = new Random();
+        String characters ="ABCDEFGHIJKLMNOPQRSTUVWXYZ"+"abcdefghijklmnopqrstuvwxyz"
+                +"0123456789"+"~`!@#$%^&*()_+-={}[]|;:'<>?,./"+"\\"+'"';
+        //System.out.println(characters.length());
+
+        StringBuffer password = new StringBuffer();
+        System.out.println();
+        for (int i = 0; i < 17; i++) {
+            password.append(characters.charAt(shuffle.nextInt(93)));
+        }
+
+        System.out.println("Password: "+password);
+    }
+}
